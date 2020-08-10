f21758d89b872760ddc9cdf9efb65821d94fcfe4
bhuma08 <bhumalimbu08@gmail.com>
2020-08-10 10:29:07 +0100

    nav link
    add nave links and it works with Route, Switch and Link

---
+++ src/Containers/about.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+class About extends Component {
+    constructor() {
+        super();
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>This is the About Page!</h1>
+            </div>
+        )
+    }
+}
+
+
+//fetch api -github
+
+export default About;
---
+++ src/Containers/shop.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+class Shop extends Component {
+    constructor() {
+        super();
+
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>This is the Shop Page!</h1>
+            </div>
+        )
+    }
+}
+
+
+
+//redux: recommended stuff- Netflix
+//redux: stuff to recommend me
+
+export default Shop;
